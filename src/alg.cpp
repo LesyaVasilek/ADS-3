3dffb4590d0d3a974f865197cfae7b59b24453ba
LesyaVasilek <125215125+LesyaVasilek@users.noreply.github.com>
2023-03-19 21:06:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int nov = 0;
+    int last = size - 1;
+    int count = 0;
+    while (nov <= last) {
+        int half = (nov + last) / 2;
+        if (arr[half] == value) {
+            count++;
+            int i = half - 1;
+            while (i >= nov && arr[i] == value) {
+                count++;
+                i--;
+            }
+            int c = half + 1;
+            while (c <= last && arr[c] == value) {
+                count++;
+                c++;
+            }
+            return count;
+        } else if (arr[half] < value) {
+            nov = half + 1;
+        } else {
+            last = half - 1;
+        }
+    }
+    return 0;
 }
